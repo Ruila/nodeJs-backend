e03200f81cbad92d628410ffb38b0c12a59ea18e
mengru <4momolmr@gmail.com>
2021-02-03 22:14:10 +0800

    succed to restruct

--- config/express.js
+++ config/express.js
@@ -20,10 +20,10 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, '../public/')));
 
 // get home page
-app.get('/', (req, res)=> {
-    console.log('in config/express');
-    res.send(`server started on port http://localhost:${port} haha`);
-})
+// app.get('/', (req, res)=> {
+//     console.log('in config/express');
+//     res.send(`server started on port http://localhost:${port} haha`);
+// })
 
 app.use('/api', index);
 
