f516ff45ce7430af6035fe4baf9abe04a30bf1fa
mengru <4momolmr@gmail.com>
2021-02-04 10:54:27 +0800

    add error middleware

--- config/express.js
+++ config/express.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var createError = require('http-errors');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
 var path = require('path');
@@ -19,12 +20,29 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, '../public/')));
 
+
 // get home page
-// app.get('/', (req, res)=> {
-//     console.log('in config/express');
-//     res.send(`server started on port http://localhost:${port} haha`);
-// })
+app.get('/', (req, res)=> {
+    console.log('in config/express');
+    res.send(`server started on port http://localhost:${port} haha`);
+})
 
 app.use('/api', index);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,16 @@ var express = require('express');
 var router = express.Router();
 var path = require('path');
 
+var user = require('./users.js');
+
 /* GET home page.-> /api */
 router.get('/', function(req, res, next) {
   res.render('index', {title: 'Express'});
   console.log('in routes/index');
   // res.render('this is: localhost:port/api');
+  next();
 });
+
+router.use('/user', user);
 
 module.exports = router;
